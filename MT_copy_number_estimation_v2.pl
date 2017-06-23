8a88a7150fb65f50db67db7457d467f60f16e7c5
Bo Shiping <boshiping@yikongenomics.com>
2017-06-23 15:34:49 +0800

    change algorithm

--- MT_copy_number_estimation_v2.pl
+++ MT_copy_number_estimation_v2.pl
@@ -6,14 +6,16 @@ use warnings;
 ##version: 2.0
 ##Author: boshiping@yikongenomics.com
 
-my $sample                    = $ARGV[0];
-my $outputfile                = $ARGV[1];
-my $autosome_count            = 0;
-my $autosome_dup_count        = 0;
-my $autosome_remove_dup_count = 0;
-my $MT_count                  = 0;
-my $MT_dup_count              = 0;
-my $MT_remove_dup_count       = 0;
+my $sample                   = $ARGV[0];
+my $outputfile               = $ARGV[1];
+my $AS_reads                 = 0;
+my $AS_dup_reads             = 0;
+my $AS_ununiq_mapped_reads   = 0;
+my $AS_rm_ununiq_dup_reads   = 0;
+my $MT_reads                 = 0;
+my $MT_dup_reads             = 0;
+my $MT_ununiq_mapped_reads   = 0;
+my $MT_rm_ununiq_dup_reads   = 0;
 
 
 open OUT, ">$outputfile" or die "Cannot open output file!";
@@ -21,22 +23,17 @@ open OUT, ">$outputfile" or die "Cannot open output file!";
 while(<STDIN>){
 	chomp;
 	my @line = split;
-	next if ($_ =~ /XT\:A\:U/);
 	if ($line[2] =~ /^(chr)?\d+$/)
 	{
-		$autosome_count++;
-		if ($line[1] >= 1024){
-			$autosome_dup_count++ ;
-		}else{
-			$autosome_remove_dup_count++;
-			}
+		$AS_reads++;
+#		$AS_dup_reads++ if ($line[1] >= 1024 );
+#		$AS_ununiq_mapped_reads++ if ($_ !~ /XT\:A\:U/ );
+		$AS_rm_ununiq_dup_reads++ if ($line[1] < 1024 && $_ =~ /XT\:A\:U/);
 	}elsif($line[2] =~ /^(chrM|MT)$/){
-		$MT_count++;
-		if ($line[1] >= 1024){
-			$MT_dup_count++ ;
-		}else{
-			$MT_remove_dup_count++;
-			}
+		$MT_reads++;
+#		$MT_dup_reads++ if ($line[1] >= 1024 );
+#		$MT_ununiq_mapped_reads++ if ($_ !~ /XT\:A\:U/ );
+		$MT_rm_ununiq_dup_reads++ if ($line[1] < 1024 && $_ =~ /XT\:A\:U/);
 		}
 	}
 
@@ -45,13 +42,13 @@ while(<STDIN>){
 #  => 
 #  mitochondrion copy number =relative amplification  efficiency *  autosome mappable region * 2 *  mitochondrion mapped reads / mitochondrion mappable region * autosome mapped reads
 
-my $autosome_mappable_region_hg19 = 2684573005;
-my $mitochondrion_mappable_region_hg19 = 16571;
+my $AS_mappable_region_hg19 = 2684573005;
+my $MT_mappable_region_hg19 = 16571;
 
-my $mitochondrion_copy_number = int ($autosome_mappable_region_hg19 * 2 * $MT_count/($mitochondrion_mappable_region_hg19 * $autosome_count));
-my $mitochondrion_copy_number_rmdup = int ($autosome_mappable_region_hg19 * 2 * $MT_remove_dup_count/($mitochondrion_mappable_region_hg19 * $autosome_remove_dup_count));
+my $MT_CN = int ($AS_mappable_region_hg19 * 2 * $MT_reads/($MT_mappable_region_hg19 * $AS_reads));
+my $MT_CN_rm_ununiq_dup = int ($AS_mappable_region_hg19 * 2 * $MT_rm_ununiq_dup_reads/($MT_mappable_region_hg19 * $AS_rm_ununiq_dup_reads));
 
 print OUT "\#Autosome_mappable_region_hg19: 2,684,573,005\n\#Mitochondrion_mappable_region_hg19: 16,571\n";
-print OUT "\#Sample\tAutosome_mapped_reads\tMitochondrion_mapped_reads\tEstimated_MT_genome_copy_number\tAutosome_remove_dup_mapped_reads\tMitochondrion_remove_dup_mapped_reads\tEstimated_MT_genome_copy_number\(remove_dup\)\n";
-print OUT "$sample\t$autosome_count\t$MT_count\t$mitochondrion_copy_number\t$autosome_remove_dup_count\t$MT_remove_dup_count\t$mitochondrion_copy_number_rmdup\n";
+print OUT "\#Sample\tAutosome_mapped_reads\tMitochondrion_mapped_reads\tEstimated_MT_genome_copy_number\tAutosome_remove_ununiq_dup_mapped_reads\tMitochondrion_remove_ununiq_dup_mapped_reads\tEstimated_MT_genome_copy_number\(remove_ununiq_dup\)\n";
+print OUT "$sample\t$AS_reads\t$MT_reads\t$MT_CN\t$AS_rm_ununiq_dup_reads\t$MT_rm_ununiq_dup_reads\t$MT_CN_rm_ununiq_dup\n";
 close OUT or die "Cannot close output file!";
